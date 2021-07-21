a22d2f5497c9308b916ab82b52a969f24b66cfb6
Romain Blary <romain.blary.uni@gmail.com>
2021-07-21 17:19:50 +0200

    Test fetch data

---
+++ public/js/data/fisheyeApi.js
@@ -0,0 +1,23 @@
+function getFishEyeData() {
+    let requestURL = "api/fisheye/fisheyeData.json";
+
+  
+    fetch(requestURL)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(function(value){
+          document.getElementById('returnTest')
+          .innerHTML = value.photographers[0];
+          console.log('value');
+      })
+      .catch(function (error) {
+        console.log("Une erreur est survenue");
+      });
+  }
+  
+  document
+  .getElementById("test")
+  .addEventListener("click", getFishEyeData);
