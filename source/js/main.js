9587f80c060a5d57c14750fb64d509283f856c07
Romain Blary <romain.blary.uni@gmail.com>
2021-07-25 16:17:30 +0200

    Fix: Ajout fichier main.js (après changement architecture dossier)

---
+++ source/js/main.js
@@ -0,0 +1,30 @@
+// FISHEYE DATA
+import getFishEyeData from "./data/fisheyeApi.js";
+import HomePageBuilder from "./modules/home/homePageBuilder.js";
+import PhotographerPageBuilder from "./modules/photographer/photographerPageBuilder.js";
+import ScrollToMainButton from "./modules/home/scroll.js";
+
+getFishEyeData()
+  // get the data and build pages and components
+  .then(function (data) {
+      //figure out if the user is one the homepage or a phtographer page
+      const homePage = document.body.classList.contains('homepage');
+      const photographerPage = document.body.classList.contains('photographer-page');
+
+      //select classes to build according to the page the user is in.
+      if(homePage){
+        new HomePageBuilder();
+        new ScrollToMainButton().displayScrollToMainBtn();
+      }else if(photographerPage){
+        new PhotographerPageBuilder();
+      }else{}
+
+  })
+  //throw error if no data loaded
+  .catch(function (error) {
+    let errorMessage = `Une erreur est survenue. \n Aucune donnée provenant de l'API FishEye n'est disponible.`;
+    let errorMessageContainer = document.querySelector(".main-content").appendChild(document.createElement("div"));
+    errorMessageContainer.classList.add("error-message");
+    errorMessageContainer.innerHTML = errorMessage;
+    console.error(errorMessage);
+  });
