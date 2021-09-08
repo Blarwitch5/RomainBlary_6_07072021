c8cde4fd579ba0ee454ba0f873bf86856b7013e4
Romain Blary <romain.blary.uni@gmail.com>
2021-09-08 16:08:57 +0200

    Ajout su style des tags photographe

--- source/js/modules/homePageBuilder.js
+++ source/js/modules/homePageBuilder.js
@@ -1,16 +1,19 @@
 /**
  * BUILD A GRID CONTAINING ALL THE PHOTOGRAPHERS BY DEFAULT
  */
+import PhotographersTags from "./components/filters.js";
+
 export default class HomePageBuilder {
   // constructor(){
-
+    
   // }
   displayPhotographersCards(data) {
     let photographers = data.photographers;
     photographers.map(function (photographer) {
       let photographersContainer = document.getElementById("photographers");
       let photographerCard = document.createElement("article");
-      photographerCard.classList.add("photographer");
+      // photographerCard.classList.add("photographer");
+      photographerCard.className = photographer.tags.join(' ') + ' photographer';
 
       let photographerMarkup = `
         <a class="photographer__link" href="/source/pages/photographer-profil.html?id=${photographer.id}" title="${photographer.name}">
@@ -22,13 +25,11 @@ export default class HomePageBuilder {
         <p class="photographer__location">${photographer.city}, ${photographer.country}</p>
         <p class="photographer__baseline">${photographer.tagline}</p>
         <p class="photographer__price">${photographer.price} € / jour</p>
-        <ul class="tags">
+        <ul class="photographer__tags tags">
         ${photographer.tags
           .map((tag) => {
             return `
-            <li class="tags__item">
-              <a href="../index.html?tag=${tag}"><span>tag </span>${tag}</a>
-            </li>`;
+            <li class="tags__item" data-filter="${tag}"><span>tag </span>#${tag}</li>`;
           })
           .join("")}
         </ul>`;
@@ -36,6 +37,5 @@ export default class HomePageBuilder {
       photographersContainer.appendChild(photographerCard);
       photographerCard.innerHTML = photographerMarkup;
     })
-    .join('');
   }
 }
