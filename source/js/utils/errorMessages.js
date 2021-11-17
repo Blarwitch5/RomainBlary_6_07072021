8aa76f10b06fe15764fc118c88bd56b29e6ce9f9
Romain Blary <romain.blary.uni@gmail.com>
2021-11-17 10:28:01 +0100

    Dev: Ajout de nouveaux messages d'erreur + création du message d'erreur

--- source/js/utils/errorMessages.js
+++ source/js/utils/errorMessages.js
@@ -1,28 +1,28 @@
 //object containing the list of error messages for each field
 export let errorMessages = {
-    firstName: {
-      specialChars: `Certains caractères spéciaux ou espaces ne sont pas admis`,
-      empty: `Le champ prénom ne doit pas être vide`,
-      length: `Le nombre de caractère du champ prénom doit  être compris entre 2 et 50`,
-    },
-    lastName: {
-      specialChars: `Certains caractères spéciaux ne sont pas admis`,
-      empty: `Le champ ne doit pas être vide`,
-      length: `Le nombre de caractère du champ nom doit  être compris entre 2 et 50`,
-    },
-    email: `Le format de l'email n'est pas valide`,
-    message: {
-      specialChars: `Certains caractères spéciaux ne sont pas admis`,
-      empty: `Le champ ne doit pas être vide`,
-      length: `Le message ne doit pas dépasser 500 caractères`,
-    },
-    termsConditions: `Pour vous inscrire, vous devez accepter les termes et conditions`,
-  };
+  firstName: {
+    specialChars: `Certains caractères spéciaux ou espaces ne sont pas admis`,
+    empty: `Le champ prénom ne doit pas être vide`,
+    length: `Le nombre de caractère du champ prénom doit être compris entre 2 et 50`,
+  },
+  lastName: {
+    specialChars: `Certains caractères spéciaux ne sont pas admis`,
+    empty: `Le champ ne doit pas être vide`,
+    length: `Le nombre de caractère du champ nom doit être compris entre 2 et 50`,
+  },
+  email: `Le format de l'email n'est pas valide`,
+  message: {
+    specialChars: `Certains caractères spéciaux ne sont pas admis`,
+    empty: `Le champ ne doit pas être vide`,
+    length: `Le message doit contenir entre 2 et 500 caractères`,
+  },
+  termsConditions: `Pour vous inscrire, vous devez accepter les termes et conditions`,
+};
 
-  //fetch data error message
-  export default function buildDataFecthingErrorMessage(){
-    let errorMessage = `Une erreur est survenue. \nAucune donnée provenant de l'API FishEye n'est disponible.`;
-    let errorMessageContainer = document.querySelector(".main-content").appendChild(document.createElement("div"));
-    errorMessageContainer.classList.add("error-message");
-    errorMessageContainer.innerHTML = errorMessage;
-  }
+//fetch data error message
+export default function buildDataFecthingErrorMessage() {
+  let errorMessage = `Une erreur est survenue. \nAucune donnée provenant de l'API FishEye n'est disponible.`;
+  let errorMessageContainer = document.querySelector(".main-content").appendChild(document.createElement("div"));
+  errorMessageContainer.classList.add("error-message");
+  errorMessageContainer.innerHTML = errorMessage;
+}
